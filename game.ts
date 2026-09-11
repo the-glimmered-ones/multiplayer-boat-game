fe136733ddd4d1d69c334cadd594d040d96f376f
the-glimmered-ones <sbthompson429@gmail.com>
2026-09-11 15:57:10 -0500

    Update game.ts

--- game.ts
+++ game.ts
@@ -27,7 +27,7 @@ export function setJoinedWithName(value: boolean){
 
 //TODO: fix loading times
 
-addEventListener("load", () => {
+document.addEventListener("load", () => {
   window.parent.alert("shart") // YAYYYYYYYYY
   //console.log("game loaded")
   //requestWs.call(window.parent, window)
